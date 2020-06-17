30b56777498aca5df8a24d9e770f126badb26ea5
Angel Cabrera <soyangelcabrera@gmail.com>
2020-06-16 21:05:33 -0400

    Conditional filters

--- src/components/PropertiesList.jsx
+++ src/components/PropertiesList.jsx
@@ -6,15 +6,16 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import PropertySlider from './PropertySlider';
+import Form from 'react-bootstrap/Form';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 import Property from './PropertiesListProperty';
 import '../assets/styles/PropertiesList.scss';
 
 const orderOptions = [
   'Relevantes',
   'Recién Publicados',
-  'Baratos',
   'Caros',
+  'Baratos',
   'Bajaron de precio',
   'Nuevos',
   'Antiguos',
@@ -22,7 +23,7 @@ const orderOptions = [
   'Pequeños',
 ];
 
-const PropertiesList = ({}) => {
+const PropertiesList = ({ openedFilters }) => {
   const [orderBy, setOrderBy] = useState('Relevantes');
 
   const handleSelect = (eventKey, event) => {
@@ -31,15 +32,6 @@ const PropertiesList = ({}) => {
 
   return (
     <>
-      <ButtonGroup className='d-none d-xl-block'>
-        <Button>1</Button>
-        <Button>2</Button>
-        <DropdownButton as={ButtonGroup} title='Dropdown'>
-          <Dropdown.Item eventKey='1'>Dropdown link</Dropdown.Item>
-          <Dropdown.Item eventKey='2'>Dropdown link</Dropdown.Item>
-        </DropdownButton>
-      </ButtonGroup>
-
       <div className='d-xl-none my-3'>
         <p>
           <b>{parseInt(Math.random() * 10000)}</b>
@@ -64,14 +56,331 @@ const PropertiesList = ({}) => {
         </DropdownButton>
       </div>
       <Row>
-        <Col xl={2}>
-          <Card className='my-3'>
+        <Col
+          xs={12}
+          xl={3}
+          className={'d-xl-block ' + (openedFilters ? 'd-block' : 'd-none')}
+        >
+          <Card className='my-3 PropertiesList__filters '>
             <Card.Body>
-              <h3>Filters</h3>
+              <div className='d-flex justify-content-between'>
+                <p style={{ 'font-size': '18px' }}>
+                  <b>Selección actual</b>
+                </p>
+                <button type='button' className='btn btn-link p-0'>
+                  <b>
+                    <i className='fas fa-trash'></i> Limpiar filtros
+                  </b>
+                </button>
+              </div>
+              <hr />
+              <div>
+                <p style={{ 'font-size': '18px' }}>
+                  <b>Ubicación</b>&nbsp;
+                </p>
+                <Form.Group controlId='formBasicSearch' className='d-flex my-3'>
+                  <Form.Control placeholder='Buscar por distrito o barrio' />
+                  <Button
+                    variant='primary btn-primary-inverse'
+                    className='ml-1'
+                  >
+                    <b>
+                      <i className='fas fa-plus'></i>
+                    </b>
+                  </Button>
+                </Form.Group>
+                <Button className='my-3' block>
+                  <i className='fas fa-map-marker-alt'></i> Ver mapa
+                </Button>
+              </div>
+              <p style={{ 'font-size': '18px' }}>
+                <b>Dirección</b>&nbsp;
+              </p>
+              <Form.Group controlId='formBasicSearch' className='d-flex my-3'>
+                <Form.Control placeholder='Buscar por dirección' />
+                <Button variant='primary btn-primary-inverse' className='ml-1'>
+                  <b>
+                    <i className='fas fa-search '></i>
+                  </b>
+                </Button>
+              </Form.Group>
+              <hr />
+              <p style={{ 'font-size': '18px' }}>
+                <b>Tipo de operación</b>&nbsp;
+              </p>
+              <div className='my-3'>
+                <Form.Check
+                  type='radio'
+                  label='Comprar'
+                  name='formHorizontalRadios'
+                  id='comprar'
+                  className='my-1'
+                />
+                <Form.Check
+                  type='radio'
+                  label='Alquilar'
+                  name='formHorizontalRadios'
+                  id='alquilar'
+                  className='my-1'
+                />
+                <Form.Check
+                  type='radio'
+                  label='Temporal/Vacacional'
+                  name='formHorizontalRadios'
+                  id='temporal'
+                  className='my-1'
+                />
+                <Form.Check
+                  type='radio'
+                  label='Proyectos'
+                  name='formHorizontalRadios'
+                  id='proyectos'
+                  className='my-1'
+                />
+              </div>
+              <hr />
+              <p style={{ 'font-size': '18px' }}>
+                <b>Precio</b>&nbsp;
+                <div className='d-flex'>
+                  <Form.Control placeholder='Desde $' />
+                  <Form.Control placeholder='Hasta $' className='mx-1' />
+                  <Button>
+                    <i className='fas fa-check'></i>
+                  </Button>
+                </div>
+              </p>
+              <hr />
+              <p style={{ 'font-size': '18px' }}>
+                <b>Condominio</b>&nbsp;
+                <div className='d-flex'>
+                  <Form.Control placeholder='Desde $' />
+                  <Form.Control placeholder='Hasta $' className='mx-1' />
+                  <Button>
+                    <i className='fas fa-check'></i>
+                  </Button>
+                </div>
+              </p>
+              <hr />
+              <p style={{ 'font-size': '18px' }}>
+                <b>Tipo de inmueble</b>&nbsp;
+              </p>
+              <div className='my-3'>
+                <Form.Check
+                  type='radio'
+                  label='Apartamento'
+                  name='formHorizontalRadios2'
+                  id='apartamento'
+                  className='my-1'
+                />
+                <Form.Check
+                  type='radio'
+                  label='Casa'
+                  name='formHorizontalRadios2'
+                  id='casa'
+                  className='my-1'
+                />
+                <Form.Check
+                  type='radio'
+                  label='Terreno'
+                  name='formHorizontalRadios2'
+                  id='terreno'
+                  className='my-1'
+                />
+                <Form.Check
+                  type='radio'
+                  label='Local comercial'
+                  name='formHorizontalRadios2'
+                  id='local'
+                  className='my-1'
+                />
+              </div>
+              <hr />
+              <p style={{ 'font-size': '18px' }}>
+                <b>Habitaciones</b>&nbsp;
+              </p>
+              <ButtonGroup className='d-flex justify-content-between' toggle>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  defaultChecked
+                  value='1'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +1
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='2'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +2
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='3'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +3
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='4'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +4
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='5'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +5
+                </ToggleButton>
+              </ButtonGroup>
+              <hr />
+              <p style={{ 'font-size': '18px' }}>
+                <b>Baños</b>&nbsp;
+              </p>
+              <ButtonGroup className='d-flex justify-content-between' toggle>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  defaultChecked
+                  value='1'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +1
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='2'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +2
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='3'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +3
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='4'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +4
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='5'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +5
+                </ToggleButton>
+              </ButtonGroup>
+              <hr />
+              <p style={{ 'font-size': '18px' }}>
+                <b>Estacionamientos</b>&nbsp;
+              </p>
+              <ButtonGroup className='d-flex justify-content-between' toggle>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  defaultChecked
+                  value='1'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +1
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='2'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +2
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='3'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +3
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='4'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +4
+                </ToggleButton>
+                <ToggleButton
+                  type='radio'
+                  name='radio'
+                  value='5'
+                  variant='primary btn-primary-inverse'
+                  className='rounded-circle'
+                >
+                  +5
+                </ToggleButton>
+              </ButtonGroup>
             </Card.Body>
           </Card>
         </Col>
-        <Col lg={12} xl={10}>
+        <Col xs={12} xl={9}>
+          <div className='d-none d-xl-flex justify-content-end my-3'>
+            <ButtonGroup>
+              <Button variant='primary btn-primary-inverse'>
+                Ordernar por:
+              </Button>
+              <Button variant='primary btn-primary-inverse'>
+                <b>Relevantes</b>
+              </Button>
+              <Button variant='primary btn-primary-inverse'>
+                Recién publicado
+              </Button>
+              <DropdownButton
+                as={ButtonGroup}
+                title={<b>Ver más</b>}
+                variant='primary btn-primary-inverse'
+              >
+                <Dropdown.Item eventKey='1'>Baratos</Dropdown.Item>
+                <Dropdown.Item eventKey='2'>Caros</Dropdown.Item>
+                <Dropdown.Item eventKey='3'>Bajaron de precio</Dropdown.Item>
+                <Dropdown.Item eventKey='4'>Amplios</Dropdown.Item>
+              </DropdownButton>
+            </ButtonGroup>
+          </div>
+          <p>
+            <b>{parseInt(Math.random() * 10000)}</b>
+            &nbsp;Casas relevantes en Venezuela
+          </p>
           <Property />
           <Property />
           <Property />
--- src/components/PropertiesListProperty.jsx
+++ src/components/PropertiesListProperty.jsx
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import PropertySlider from './PropertySlider';
 
 const images = [
@@ -13,47 +14,61 @@ const images = [
 ];
 
 const PropertiesListProperty = () => (
-  <Card className='PropertiesList__card my-3'>
-    <Row>
-      <Col xs={12} md={4}>
-        <Card>
-          <PropertySlider images={images}></PropertySlider>
-          <div className='p-3'>
+  <Card className='PropertiesListProperty my-3'>
+    <Container>
+      <Row>
+        <Col xs={12} md={5} className='px-0'>
+          <Card>
+            <PropertySlider images={images}></PropertySlider>
+            <div className='p-3'>
+              <h3 className='font-weight-normal'>
+                <b>US$ {parseInt(Math.random() * 10000)}</b>
+              </h3>
+            </div>
+          </Card>
+        </Col>
+        <Col xs={12} md={7}>
+          <div className='p-2 d-flex flex-column justify-content-between h-100'>
             <h5>
-              <b>US$ {parseInt(Math.random() * 10000)}</b>
+              <a href='#'>Apartamento Consolitex Vende La Trigaleña Mirian</a>
             </h5>
-          </div>
-        </Card>
-      </Col>
-      <Col xs={12} md={8}>
-        <div className='p-3'>
-          <h5>
-            <a href='#'>Apartamento Consolitex Vende La Trigaleña Mirian</a>
-          </h5>
-          <p className='mb-2'>URB LA TRIGALEÑA, La Trigaleña, Valencia</p>
-          <p>
-            <b>
-              106 m² totales&nbsp;&nbsp; 106 m² construidos&nbsp;&nbsp; 3
-              Habitaciones&nbsp;&nbsp; 4 Baños &nbsp;&nbsp; 2 Estacionamientos
-            </b>
-          </p>
-          <p className='text-muted'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consequuntur eveniet, optio perferendis tempora ut facere
-            voluptatibus, asperiores veritatis rerum praesentium numquam
-            consequatur est ipsum culpa distinctio quibusdam animi fuga
-            aspernatur!
-          </p>
-          <div class='d-flex justify-content-between align-items-center mt-3'>
-            <p className='text-muted'>
-              <i className='fas fa-sync'></i> Publicado hace&nbsp;
-              {parseInt(Math.random() * 100)} dias
+            <p className='mb-2'>URB LA TRIGALEÑA, La Trigaleña, Valencia</p>
+            <p>
+              <b>
+                106 m² totales&nbsp;&nbsp; 106 m² construidos&nbsp;&nbsp; 3
+                Habitaciones&nbsp;&nbsp; 4 Baños &nbsp;&nbsp; 2 Estacionamientos
+              </b>
             </p>
-            <Button variant='primary'>Contactar</Button>
+            <p
+              className='text-muted'
+              style={{
+                'max-height': '3rem',
+                overflow: 'hidden',
+                'font-size': '12px',
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequuntur eveniet, optio perferendis tempora ut facere
+              voluptatibus, asperiores veritatis rerum praesentium numquam
+              consequatur est ipsum culpa distinctio quibusdam animi fuga
+              aspernatur!
+            </p>
+            <div class='d-flex justify-content-between align-items-center mt-3'>
+              <p className='text-muted'>
+                <i className='fas fa-sync'></i> Publicado hace&nbsp;
+                {parseInt(Math.random() * 100)} dias
+              </p>
+              <Button
+                variant='primary'
+                className='PropertiesListProperty__contact'
+              >
+                Contactar
+              </Button>
+            </div>
           </div>
-        </div>
-      </Col>
-    </Row>
+        </Col>
+      </Row>
+    </Container>
   </Card>
 );
 
