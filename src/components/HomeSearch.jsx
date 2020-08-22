cbffd093f96aca2e187fcbca7fd99df8f463d635
Angel Cabrera <soyangelcabrera@gmail.com>
2020-08-22 15:26:10 -0400

    remove border for input

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -4,7 +4,8 @@ import '../assets/styles/HomeSearch.scss';
 const Layout = () => (
   <div className='Search'>
     <div className="Search__bar">
-      <input type="text" className="Search__bar-input" placeholder="Buscar por ubicación" />
+      <input type="text" className="Search__bar-input" placeholder="Buscar por ubicación"
+      style="border: none!important" />
       <a href="/propiedades" className="Search__bar-button">
         <i className="fas fa-search mr-2"></i>Buscar
       </a>
